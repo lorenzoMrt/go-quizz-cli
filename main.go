dfb11405a6cbc2a2327f8056d375d455273e1a79
lorenzoMrt <lorenzomirt@gmail.com>
2025-01-12 13:19:45 +0100

    [feat] quiz without timer

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Quiz struct {
+	Question string
+	Answer   string
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	file, err := os.Open("internal/questions.csv")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := bufio.NewScanner(os.Stdin)
+	quizzes := createQuizzes(data)
+	correctAnswers := 0
+	for _, quiz := range quizzes {
+		fmt.Println(quiz.Question)
+		input.Scan()
+		if input.Text() == quiz.Answer {
+			correctAnswers++
+		}
+	}
+	score := (float64(correctAnswers) / float64(len(quizzes))) * 100
+	fmt.Printf("%.2f\n", score)
+}
+
+func createQuizzes(data [][]string) []Quiz {
+	var quizzes []Quiz
+	for _, question := range data {
+		var quiz Quiz
+		for i, field := range question {
+			if i == 0 {
+				quiz.Question = field
+			} else if i == 1 {
+				quiz.Answer = field
+			}
+		}
+		quizzes = append(quizzes, quiz)
+	}
+	return quizzes
 }
